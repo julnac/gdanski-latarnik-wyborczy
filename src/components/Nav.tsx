61f17260011094132440203d98fd562cd765cb8e
Julia <julia.naczke007@gmail.com>
2024-03-10 17:01:57 +0100

    add gh-pages

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,19 +3,28 @@ import "../styles/nav.scss";
 import logo from "../assets/vote.png";
 
 const Nav = () => {
+
+    const handleScrollToSection = (sectionId: string) => {
+        const element = document.getElementById(sectionId);
+        if (element) {
+          element.scrollIntoView({ behavior: 'smooth' });
+        }
+      };
+
     return (
         <nav id="nav">
             <div className="logo">
                 <img src={logo} alt="logo"/>
-                <a href="#Hero">Gdański Latarnik Wyborczy</a>
+                <a onClick={() => handleScrollToSection("Hero")}>Gdański Latarnik Wyborczy</a>
             </div>
             <div className="options">
-                <ul>
-                    <li><a href="#Onas">O latarniku</a></li>
-                    <li><a href="#Faq">FAQ</a></li>
-                    <li><a href="#Twórcy">Twórcy</a></li>
-                    <li><a href="#Kontakt">Kontakt</a></li>
-                </ul>
+            <ul>
+                <li><a onClick={() => handleScrollToSection("Onas")}>O latarniku</a></li>
+                <li><a onClick={() => handleScrollToSection("Faq")}>FAQ</a></li>
+                <li><a onClick={() => handleScrollToSection("Twórcy")}>Twórcy</a></li>
+                <li><a onClick={() => handleScrollToSection("Kontakt")}>Kontakt</a></li>
+            </ul>
+
                 <Link to="/Form"><button className="button">Rozpocznij test</button></Link>
             </div>
         </nav>
